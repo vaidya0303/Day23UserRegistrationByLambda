9f3cd3aff8adc4883dc493c8a4adcd6f21ff883d
vaidya0303 <vaidya0304@gmail.com>
2022-11-11 20:56:34 +0530

    Added:To Validate Last Name.UC2.

--- src/com/day23/UserRegistration.java
+++ src/com/day23/UserRegistration.java
@@ -2,6 +2,8 @@ package com.day23;
 /**
  * UC1 :- As a User need to enter a valid First Name
  *        - First name starts with Cap and has minimum 3 characters
+ * UC2 :- As a User need to enter a valid Last Name
+ *        - Last name starts with Cap and has minimum 3 character
  */
 
 /**
@@ -18,29 +20,58 @@ public class UserRegistration {
     /**
      * Create a method name as firstName, this method is boolean type than means its return op is true or false
      * type, also this is parameterized method
+     *
      * @param firstName
-     * @return firstname
+     * @return firstname matched the regex
      */
     public boolean firstName(String firstName) {
         /**
          * Regex to check valid firstname
          * 1) First name starts with Cap and has minimum 3 characters
          */
-        String  regex = "^[A-Z]{1}[a-z]{3,}";
+        String regex = "^[A-Z]{1}[a-z]{3,}";
         /**
          * First, the pattern is created using the Pattern.compile() method
          * The first parameter indicates which pattern is being searched for
          */
-        Pattern pattern= Pattern.compile(regex);
+        Pattern pattern = Pattern.compile(regex);
         /**
          * The matcher() method is used to search for the pattern in a string.
          * It returns a Matcher object which contains information about the search that was performed.
          */
         Matcher match = pattern.matcher(firstName);
         /**
-         * for return op is true or false
+         * Return if the firsName matched the Regex
          */
         return match.matches();
+    }
+
+    /**
+     * Create a method name as lastName, this method is boolean type than means its return op is true or false
+     * type, also this is parameterized method
+     * @param lastName
+     * @return userlastname matched the Regex
+     */
+
+    public boolean lastName(String lastName) {
+        /**
+         *  Regex to check valid userlastname.
+         *  1) Last name starts with Cap and has minimum 3 character
+         */
+        String regex = "^[A-Z]{1}[a-z]{3,}";
+        /**
+         *  Compile the Regex
+         */
+        Pattern pattern = Pattern.compile(regex);
+        /**
+         * Pattern class contains matcher() method to find matching between given userlastname
+         * and regular expression.
+         */
+        Matcher matcher = pattern.matcher(lastName);
+        /**
+         * Return if the userlastname matched the Regex
+         */
+        return matcher.matches();
     }
 
 }
--- src/com/day23/UserRegistrationTest.java
+++ src/com/day23/UserRegistrationTest.java
@@ -7,7 +7,9 @@ public class UserRegistrationTest {
 
     UserRegistration userRegistration = new UserRegistration();
 
-
+    /**
+     * test case for uc 1
+     */
     @Test
     public void givenFirstName_WhenInFormat_ShouldReturnTrue() {
         boolean result = userRegistration.firstName("Nilofar");
@@ -20,5 +22,20 @@ public class UserRegistrationTest {
         Assertions.assertEquals(false, result);
     }
 
+    /**
+     * test case for uc2
+     */
+    @Test
+    public void givenLastName_WhenInFormat_ShouldReturnTrue() {
+        boolean result = userRegistration.lastName("Mujawar");
+        Assertions.assertEquals(true, result);
+    }
+
+    @Test
+    public void givenLastName_WhenOurOfFormat_ShouldReturnFalse() {
+        boolean result = userRegistration.lastName("mujawar");
+        Assertions.assertEquals(false, result);
+
+    }
 
 }
